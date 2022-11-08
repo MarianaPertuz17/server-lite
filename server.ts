8ebe41ec1f28e47edff88ae6f01ab0a95cd8f1f6
MarianaPertuz17 <marianapertuzv@gmail.com>
2022-11-07 22:23:57 -0500

    feat: adding tslint configs

--- server.ts
+++ server.ts
@@ -1,16 +1,16 @@
-import { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const express = require('express');
 const app = express();
 const PORT: number = process.env.PORT || 5000;
-app.use(express.json())
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+
+app.use(express.json());
+app.get('/', (_: Request, res: Response) => {
+  res.send('Hello World!');
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
-})
+  console.log(`Server is listening on port ${PORT}`); // tslint:disable-line
+});
