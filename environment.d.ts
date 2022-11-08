01831075d32e168792b8a728d40d952fa7e2d601
MarianaPertuz17 <marianapertuzv@gmail.com>
2022-11-07 23:29:59 -0500

    feat: base sequelize setup with .env.example

--- environment.d.ts
+++ environment.d.ts
@@ -1,7 +1,14 @@
+import { Dialect } from "sequelize";
+
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PORT: number;
+      DB_NAME: string; 
+      DB_USER: string; 
+      DB_PASSWORD: string;
+      DB_HOST: string; 
+      DB_DIALECT: Dialect;
     }
   }
 }
---
+++ models/index.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME ?? 'my_db',
+  process.env.DB_USER ?? 'my_user',
+  process.env.DB_PASSWORD ?? '',
+  {
+    host: process.env.DB_HOST ?? 'localhost',
+    dialect: process.env.DB_DIALECT ?? 'postgres',
+    logging: true,
+  }
+);
