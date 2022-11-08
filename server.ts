16c3c823eeadde2afa39e7a3641a42898bc783a3
MarianaPertuz17 <marianapertuzv@gmail.com>
2022-11-07 21:49:29 -0500

    feat: heroku env variable

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const express = require('express');
-const app = express()
-const PORT = 5000
+const app = express();
+const PORT: number = process.env.PORT || 5000;
 app.use(express.json())
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
