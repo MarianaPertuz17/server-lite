85cdbd82a8f94e2029c0f2c1c3bde74259f87352
MarianaPertuz17 <marianapertuzv@gmail.com>
2022-11-09 00:24:09 -0500

    feat: editing companies

--- controllers/admin.ts
+++ controllers/admin.ts
@@ -2,18 +2,18 @@ import { Request, Response } from 'express';
 import { Company } from '../models/company';
 import { NITValidator, phoneValidator } from '../utils';
 
-interface ICreateCompanyBody {
+interface ICompanyBody {
   NIT: string;
   name: string;
   address: string;
   phone: string;
 }
 
-interface ICreateCompanyRequest extends Request {
-  body: ICreateCompanyBody;
+interface ICompanyRequest extends Request {
+  body: ICompanyBody;
 }
 
-const createCompany = async (req: ICreateCompanyRequest, res: Response) => {
+const createCompany = async (req: ICompanyRequest, res: Response) => {
   try {
     const { NIT, name, address, phone } = req.body;
 
@@ -39,4 +39,30 @@ const createCompany = async (req: ICreateCompanyRequest, res: Response) => {
   }
 };
 
-export { createCompany };
+const editCompany = async (req: ICompanyRequest, res: Response) => {
+  try {
+    const { NIT, name, address, phone } = req.body;
+
+    if (!NIT || !name || !address || !phone) {
+      return res.status(400).send({res: 'Missing form fields!', error: true});
+    }
+
+    if (!phoneValidator(phone)) return res.status(400).send({res: 'Expected phone number of 10 digits', error: true});
+
+    if (!NITValidator(NIT)) return res.status(400).send({res: 'We only accept numbers in NIT', error: true});
+
+    const company = await Company.findOne({where: {NIT}});
+
+    if (!company) return res.status(409).send({res: 'Company with that NIT does not exist', error: true});
+
+    const editedCompany = await Company.update({NIT, name, address, phone}, {where: { NIT }, returning: true});
+
+    return res.status(200).send({res: editedCompany[1][0], error: false});
+
+  } catch (e) {
+    console.log(e); // tslint:disable-line
+    res.status(500).send({ res: 'Internal Server Error!', error: true });
+  }
+};
+
+export { createCompany, editCompany };
--- router/admin.ts
+++ router/admin.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createCompany } from '../controllers/admin';
+import { createCompany, editCompany } from '../controllers/admin';
 import { loginAdmin, registerAdmin } from '../controllers/auth';
 import { adminMiddleware } from '../middleware/admin';
 import { authMiddleware } from '../middleware/auth';
@@ -12,5 +12,6 @@ adminRouter.post('/register', registerAdmin);
 
 // company
 adminRouter.post('/company', authMiddleware, adminMiddleware, createCompany);
+adminRouter.put('/company', authMiddleware, adminMiddleware, editCompany);
 
 export { adminRouter };
