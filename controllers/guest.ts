b96bd2ce5872414e20fb671001e4ecb7c48ba0c0
MarianaPertuz17 <marianapertuzv@gmail.com>
2022-11-09 13:44:49 -0500

    feat: company and company details by id

--- controllers/guest.ts
+++ controllers/guest.ts
@@ -1,14 +1,32 @@
 import { Request, Response } from 'express';
-import { Admin } from '../models/admin';
+import { Company } from '../models/company';
 
-const getUserInfo = async (_: Request, res: Response) => {
+interface ICompanyGetRequest extends Request {
+  params: {
+    companyId: string;
+  };
+}
+
+const getCompanies = async (_: Request, res: Response) => {
   try {
-    const user = await Admin.findOne({where: {email: 'MariPer'}});
-    res.status(200).json({ res: user, error: false });
+    const companies = await Company.findAll();
+    res.status(200).json({ res: companies, error: false });
   } catch (e) {
     console.log(e); // tslint:disable-line
     res.status(500).send({ res: 'Internal Server Error!', error: true });
   }
 };
 
-export { getUserInfo };
+const getCompany = async (req: ICompanyGetRequest, res: Response) => {
+  try {
+    const { companyId } = req.params;
+    const company = await Company.findOne({where: {NIT: companyId}, include: 'products'});
+    res.status(200).json({ res: company, error: false });
+  } catch (e) {
+    console.log(e); // tslint:disable-line
+    res.status(500).send({ res: 'Internal Server Error!', error: true });
+  }
+};
+
+
+export { getCompanies, getCompany };
--- router/guest.ts
+++ router/guest.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { loginGuest, registerGuest } from '../controllers/auth';
+import { getCompanies, getCompany } from '../controllers/guest';
+import { authMiddleware } from '../middleware/auth';
 
 const guestRouter = Router();
 
 guestRouter.post('/login', loginGuest);
 guestRouter.post('/register', registerGuest);
+
+// visualize companies
+guestRouter.get('/companies', authMiddleware, getCompanies);
+guestRouter.get('/company/:companyId', authMiddleware, getCompany);
 
 export { guestRouter };
